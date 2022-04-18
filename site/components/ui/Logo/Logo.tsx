9ca0f8b2040629d0148837d240aa8c9b9b6aab40
greyhere <57859708+greyhere@users.noreply.github.com>
2022-04-19 02:35:59 +0530

    replace png logo with svg

--- site/components/ui/Logo/Logo.tsx
+++ site/components/ui/Logo/Logo.tsx
@@ -1,8 +1,29 @@
-import Image from 'next/image';
-
-// TODO: replace with svg
-const Logo = () => (
-  <Image height='44.16px' width='142px' src='/logo.png' alt='logo' />
+const Logo = ({ stroke = 'currentColor', ...props }) => (
+  <svg
+    width='203'
+    height='64'
+    viewBox='0 0 203 64'
+    fill='none'
+    stroke={stroke}
+    shapeRendering='geometricPrecision'
+    {...props}
+  >
+    <path
+      d='M1 60.8821V3L31.7681 32.719L62.5362 3H200L139.953 61H202'
+      stroke={stroke}
+      strokeWidth='2'
+    />
+    <path
+      d='M98.75 61H64V39M64 39V17H88M64 39H88'
+      stroke={stroke}
+      strokeWidth='1.5'
+    />
+    <path
+      d='M98 61.5V16L128.844 61V16H186.513L152.5 48.7273H202'
+      stroke={stroke}
+      strokeWidth='1.5'
+    />
+  </svg>
 );
 
 export default Logo;
