68ee71c2fa3d5bb47c1577bb2706d482ffc0dd15
greyhere <57859708+greyhere@users.noreply.github.com>
2022-04-19 15:04:10 +0530

    remove unnecessary attributes from `Logo`

--- site/components/ui/Logo/Logo.tsx
+++ site/components/ui/Logo/Logo.tsx
@@ -10,17 +10,11 @@ const Logo = ({ stroke = 'currentColor', ...props }) => (
   >
     <path
       d='M1 60.8821V3L31.7681 32.719L62.5362 3H200L139.953 61H202'
-      stroke={stroke}
       strokeWidth='2'
     />
-    <path
-      d='M98.75 61H64V39M64 39V17H88M64 39H88'
-      stroke={stroke}
-      strokeWidth='1.5'
-    />
+    <path d='M98.75 61H64V39M64 39V17H88M64 39H88' strokeWidth='1.5' />
     <path
       d='M98 61.5V16L128.844 61V16H186.513L152.5 48.7273H202'
-      stroke={stroke}
       strokeWidth='1.5'
     />
   </svg>
